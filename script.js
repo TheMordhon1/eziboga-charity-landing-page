953ec0745eb20f96368f556df3ec669dc417b0a7
Muhamad Romdon <52556786+TheMordhon1@users.noreply.github.com>
2020-04-11 15:59:35 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let rellax = new Rellax('.rellax');
+let rellax = new Rellax(".rellax");
 
 // Accordion
 $(".header").click(function () {
   $(".accordion-item").removeClass("active");
   $(this).parent().addClass("active");
   $(".icon").text("+");
-  $(this).children(".icon").text("-")
+  $(this).children(".icon").text("-");
 });
